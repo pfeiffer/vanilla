fb95e6922dc7377d199273e72e0ef444c08aba00
Mattias Pfeiffer <mattias@pfeiffer.dk>
2015-05-29 11:31:29 +0200

    Symlink resque-pool.yml from shared.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -6,3 +6,11 @@ Chef::Log.info("Symlinking env files...")
 link "#{release_path}/.env" do
   to "#{new_resource.deploy_to}/shared/config/env.env"
 end
+
+Chef::Log.info("Symlinking config files...")
+
+%w(resque-pool.yml).each do |file|
+  link "#{release_path}/config/#{file}" do
+    to "#{new_resource.deploy_to}/shared/config/#{file}"
+  end
+end
